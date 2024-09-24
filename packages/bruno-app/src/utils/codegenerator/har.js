fad7c30293e7fdb2e80d988b500b68f5bd2b649a
Sanjai Kumar <sanjailucifer666@gmail.com>
2024-09-24 13:23:29 +0530

    Improvements on code export feature. Now the graphql request can also be exported as code

--- packages/bruno-app/src/utils/codegenerator/har.js
+++ packages/bruno-app/src/utils/codegenerator/har.js
@@ -1,70 +1,83 @@
-const createContentType = (mode) => {
-  switch (mode) {
-    case 'json':
-      return 'application/json';
-    case 'text':
-      return 'text/plain';
-    case 'xml':
-      return 'application/xml';
-    case 'sparql':
-      return 'application/sparql-query';
-    case 'formUrlEncoded':
-      return 'application/x-www-form-urlencoded';
-    case 'graphql':
-      return 'application/json';
-    case 'multipartForm':
-      return 'multipart/form-data';
-    default:
-      return '';
+const getContentType = (mode) => {
+  const contentTypes = {
+    json: 'application/json',
+    text: 'text/plain',
+    xml: 'application/xml',
+    sparql: 'application/sparql-query',
+    formUrlEncoded: 'application/x-www-form-urlencoded',
+    graphql: 'application/json',
+    multipartForm: 'multipart/form-data'
+  };
+  return contentTypes[mode] || '';
+};
+
+export const parseGraphQLRequestBody = (request) => {
+  if (request?.body?.mode === 'graphql' && request.body?.graphql) {
+    try {
+      const { graphql } = request.body;
+
+      if (typeof graphql.variables === 'string') {
+        graphql.variables = JSON.parse(graphql.variables);
+      }
+
+      request.body.graphql = {
+        query: graphql.query,
+        variables: graphql.variables
+      };
+    } catch (e) {
+      console.error('Failed to parse GraphQL variables', e);
+    }
   }
 };
 
 const createHeaders = (request, headers) => {
-  const enabledHeaders = headers
-    .filter((header) => header.enabled)
-    .map((header) => ({
-      name: header.name,
-      value: header.value
-    }));
+  const enabledHeaders = headers.filter((header) => header.enabled).map(({ name, value }) => ({ name, value }));
 
-  const contentType = createContentType(request.body?.mode);
-  if (contentType !== '') {
+  const contentType = getContentType(request.body?.mode);
+  if (contentType) {
     enabledHeaders.push({ name: 'content-type', value: contentType });
   }
   return enabledHeaders;
 };
 
-const createQuery = (queryParams = []) => {
-  return queryParams
-    .filter((param) => param.enabled && param.type === 'query')
-    .map((param) => ({
-      name: param.name,
-      value: param.value
-    }));
-};
+const createQuery = (queryParams = []) =>
+  queryParams.filter((param) => param.enabled && param.type === 'query').map(({ name, value }) => ({ name, value }));
 
 const createPostData = (body) => {
-  const contentType = createContentType(body.mode);
+  const contentType = getContentType(body.mode);
+
+  if (body.mode === 'graphql' && body.graphql) {
+    return {
+      mimeType: 'application/json',
+      text: JSON.stringify({
+        query: body.graphql.query || '', //This could break if no query values?
+        variables: body.graphql.variables || {}
+      })
+    };
+  }
+
   if (body.mode === 'formUrlEncoded' || body.mode === 'multipartForm') {
     return {
       mimeType: contentType,
       params: body[body.mode]
         .filter((param) => param.enabled)
-        .map((param) => ({
-          name: param.name,
-          value: param.value,
-          ...(param.type === 'file' && { fileName: param.value })
+        .map(({ name, value, type }) => ({
+          name,
+          value,
+          ...(type === 'file' && { fileName: value })
         }))
     };
-  } else {
-    return {
-      mimeType: contentType,
-      text: body[body.mode]
-    };
   }
+
+  return {
+    mimeType: contentType,
+    text: body[body.mode]
+  };
 };
 
 export const buildHarRequest = ({ request, headers }) => {
+  parseGraphQLRequestBody(request);
+
   return {
     method: request.method,
     url: encodeURI(request.url),
